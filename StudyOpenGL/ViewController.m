b8361fe7a8d8e2caf6982f6384f5829851499a34
ogata <shingo.ogata@jrits.ricoh.co.jp>
2014-05-23 20:48:16 +0900

    [133] GLKReflectionMapEffect で環境マッピングを実現
    * うまくできていない。オブジェクトとテクスチャがずれる

--- StudyOpenGL/ViewController.m
+++ StudyOpenGL/ViewController.m
@@ -10,7 +10,7 @@
 @interface ViewController()
 {
     float mTime;
-    GLKBaseEffect* mEffect;
+    GLKReflectionMapEffect * mEffect;
     GLuint mBuffer;
     GLuint cBuffer;
 }
@@ -19,141 +19,128 @@
 @implementation ViewController
 - (void) viewDidAppear:(BOOL)animated
 {
+    
     // GLKViewController を使う準備
     [super viewDidAppear:animated];
+    self.view.backgroundColor = [UIColor redColor];
     ((GLKView *)self.view).context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2]; //OpenGL ES 2.0になる
     EAGLContext.currentContext = ((GLKView*)self.view).context;
-    mEffect = [[GLKBaseEffect alloc] init];
-    // ------------
-    
-    glGenBuffers(1, &mBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, mBuffer);
-    static	float	sVerts[] ={/*
-     -1, -1, -1,//0
-     1, -1, -1,//1
-     1, 1, -1,//2
-     -1, 1, -1,//3
-     -1, -1, 1,//4
-     1, -1, 1,//5
-     1, 1, 1,//6
-     -1, 1, 1,//7
-                                */
-     -1, -1, -1,//0
-     1, -1, -1,//1
-     -1, 1, -1,//3
-     1, 1, -1,//2
-        
-     1, 1, -1,//2
-     -1, 1, -1,//3
-     1, 1, 1,//6
-     -1, 1, 1,//7
-        
-     -1, 1, 1,//7
-     -1, -1, 1,//4
-     1, 1, 1,//6
-     1, -1, 1,//5
-        
-     1, -1, 1,//5
-     -1, -1, 1,//4
-     1, -1, -1,//1
-     -1, -1, -1,//0
-        
-     -1, -1, -1,//0
-     -1, -1, 1,//4
-     -1, 1, -1,//3
-     -1, 1, 1,//7
-        
-     -1, 1, 1,//7
-     -1, -1, 1,//4
-     1, 1, 1,//6
-     1, -1, 1,//5
-        
-     1, -1, 1,//5
-     1, 1, 1,//6
-     1, -1, -1,//1
-     1, 1, -1,//2
-        
-        
-        
-    };
-    
-    /*    {	-1,	-1,	-1, //p1
-       -1,	-1,	1, //p2
-        -1,	1,	-1,//p3
-        -1,	1,	1, //p4
-        
-        -1,	-1,	-1, //p1
-        -1,	1,	-1, //p3
-        1,	-1,	-1, //p5
-        1,	1,	-1, //p7
-        
-        -1,	-1,	-1, //p1
-        1,	-1,	-1, //p5
-       -1,	-1,	1, //p2
-        1,	-1,	1, //p6
-        
-       1,	1,	1, //p8
-        1,	-1,	1, //p6
-        1,	1,	-1, //p7
-        1,	-1,	-1, //p5
-        
-       1,	1,	1, //p8
-        -1,	1,	1, //p4
-        1,	-1,	1, //p6
-       -1,	-1,	1, //p2
-        
-       1,	1,	1, //p8
-        1,	1,	-1, //p7
-        -1,	1,	1, //p4
-        -1,	1,	-1,//p3
-    };*/
-    glBufferData(GL_ARRAY_BUFFER, sizeof(sVerts), sVerts, GL_STATIC_DRAW);
-    
-    static	GLKVector4	sColors[] =
-    {
-        {	 1,	 0,	 0,	 1	}
-		,	{	 1,	 0,	 0,	 1	}
-		,	{	 1,	 0,	 0,	 1	}
-		,	{	 1,	 0,	 0,	 1	}
-        
-		,	{	 0,	 0,	 1,	 1	}
-		,	{	 0,	 0,	 1,	 1	}
-		,	{	 0,	 0,	 1,	 1	}
-		,	{	 0,	 0,	 1,	 1	}
-        
-		,	{	 0,	 1,	 0,	 1	}
-		,	{	 0,	 1,	 0,	 1	}
-		,	{	 0,	 1,	 0,	 1	}
-		,	{	 0,	 1,	 0,	 1	}
-        
-		,	{	 0,	 1,	 1,	 1	}
-		,	{	 0,	 1,	 1,	 1	}
-		,	{	 0,	 1,	 1,	 1	}
-		,	{	 0,	 1,	 1,	 1	}
-        
-		,	{	 1,	 1,	 0,	 1	}
-		,	{	 1,	 1,	 0,	 1	}
-		,	{	 1,	 1,	 0,	 1	}
-		,	{	 1,	 1,	 0,	 1	}
-        
-       ,    {	 1,	 0,	 0,	 0	}
-		,	{	 1,	 0,	 0,	 0	}
-		,	{	 1,	 0,	 0,	 0	}
-		,	{	 1,	 0,	 0,	 0	}
-        
-       ,    {	 1,	 1,	 1,	 1	}
-		,	{	 1,	 1,	 1,	 1	}
-		,	{	 1,	 1,	 1,	 1	}
-		,	{	 1,	 1,	 1,	 1	}
-    };
-    
-    glGenBuffers(1, &cBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, cBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(sColors), sColors, GL_STATIC_DRAW);
-    
     // 色々考えろ。と伝えている箇所
     ((GLKView*)self.view).drawableDepthFormat = GLKViewDrawableDepthFormat24;
 	glEnable( GL_DEPTH_TEST );
+  NSArray * paths =
+     @[ [ NSBundle.mainBundle pathForResource:@"R" ofType:@"png" ]
+        ,	[ NSBundle.mainBundle pathForResource:@"L" ofType:@"png" ]
+        ,	[ NSBundle.mainBundle pathForResource:@"T" ofType:@"png" ]
+        ,	[ NSBundle.mainBundle pathForResource:@"B" ofType:@"png" ]
+        ,	[ NSBundle.mainBundle pathForResource:@"N" ofType:@"png" ]
+        ,	[ NSBundle.mainBundle pathForResource:@"F" ofType:@"png" ]
+        ];
+    GLKTextureInfo * wTI= [GLKTextureLoader cubeMapWithContentsOfFiles:paths
+                                                                options:nil
+                                                                  error:NULL
+                            ];
+    assert(wTI);
+    
+	mEffect = [ [ GLKReflectionMapEffect alloc ] init ];
+	mEffect.textureCubeMap.name = wTI.name;
+    
+    // ------------
+    
+    static GLfloat sCubeVertexData[ 108 ] =
+    {	 1, -1, -1
+		,	 1,  1, -1
+		,	 1, -1,  1
+		,	 1, -1,  1
+		,	 1,  1, -1
+		,	 1,  1,  1
+        
+		,	 1,  1, -1
+		,	-1,  1, -1
+		,	 1,  1,  1
+		,	 1,  1,  1
+		,	-1,  1, -1
+		,	-1,  1,  1
+        
+		,	-1,  1, -1
+		,	-1, -1, -1
+		,	-1,  1,  1
+		,	-1,  1,  1
+		,	-1, -1, -1
+		,	-1, -1,  1
+        
+		,	-1, -1, -1
+		,	 1, -1, -1
+		,	-1, -1,  1
+		,	-1, -1,  1
+		,	 1, -1, -1
+		,	 1, -1,  1
+        
+		,	 1,  1,  1
+		,	-1,  1,  1
+		,	 1, -1,  1
+		,	 1, -1,  1
+		,	-1,  1,  1
+		,	-1, -1,  1
+        
+		,	 1, -1, -1
+		,	-1, -1, -1
+		,	 1,  1, -1
+		,	 1,  1, -1
+		,	-1, -1, -1
+		,	-1,  1, -1
+    };
+    static	GLfloat sCubeNormalData[ 108 ] =
+    {	 1,	 0,	 0
+		,	 1,	 0,	 0
+		,	 1,	 0,	 0
+		,	 1,	 0,	 0
+		,	 1,	 0,	 0
+		,	 1,	 0,	 0
+		
+		,	 0,	 1,	 0
+		,	 0,	 1,	 0
+		,	 0,	 1,	 0
+		,	 0,	 1,	 0
+		,	 0,	 1,	 0
+		,	 0,	 1,	 0
+        
+		,	-1,	 0,	 0
+		,	-1,	 0,	 0
+		,	-1,	 0,	 0
+		,	-1,	 0,	 0
+		,	-1,	 0,	 0
+		,	-1,	 0,	 0
+        
+		,	 0,	-1,	 0
+		,	 0,	-1,	 0
+		,	 0,	-1,	 0
+		,	 0,	-1,	 0
+		,	 0,	-1,	 0
+		,	 0,	-1,	 0
+        
+		,	 0,	 0,	 1
+		,	 0,	 0,	 1
+		,	 0,	 0,	 1
+		,	 0,	 0,	 1
+		,	 0,	 0,	 1
+		,	 0,	 0,	 1
+        
+		,	 0,	 0,	-1
+		,	 0,	 0,	-1
+		,	 0,	 0,	-1
+		,	 0,	 0,	-1
+		,	 0,	 0,	-1
+		,	 0,	 0,	-1
+    };
+    glGenBuffers(1, &mBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, mBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(sCubeVertexData), sCubeVertexData, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &cBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, cBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(sCubeNormalData), sCubeNormalData, GL_STATIC_DRAW);
+    
 }
 
 - (void) viewWillDisappear:(BOOL)animated
@@ -170,7 +157,7 @@
 
 - (void) update
 {
-    mTime = mTime + self.timeSinceLastUpdate * 2;
+    mTime += self.timeSinceLastUpdate;
 }
 
 //GLKBaseEffect を使って３角形を描画する。(3次元にお試し改良)
@@ -178,30 +165,37 @@
 - (void) glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    mEffect.transform.modelviewMatrix = GLKMatrix4Rotate(	GLKMatrix4MakeScale( .5, .5, .5 )
+    glClearColor(0, 0, 0, 1);
+    mEffect.transform.modelviewMatrix = GLKMatrix4Rotate(	GLKMatrix4MakeTranslation( 0, 0, -4 )
                                                                 ,	mTime
+                                                                ,	-1
                                                                 ,	1
-                                                                ,	1
-                                                                ,	1
+                                                                ,	-1
                                                                 );
-    mEffect.transform.projectionMatrix = GLKMatrix4MakeScale( 1, view.bounds.size.width / view.bounds.size.height, 1 );
+    mEffect.transform.projectionMatrix = GLKMatrix4MakePerspective
+	(	GLKMathDegreesToRadians( 90 )
+     ,	self.view.bounds.size.width / self.view.bounds.size.height
+     ,	1
+     ,	100
+     );
+
     [mEffect prepareToDraw];
 
-    [self draw];
+    [self draw:GLKVertexAttribPosition normal:GLKVertexAttribNormal];
 }
-- (void) draw
+- (void) draw:(GLuint) position normal:(GLuint) normal
 {
     glBindBuffer( GL_ARRAY_BUFFER, mBuffer );
-    glVertexAttribPointer( GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0 );
-    glBindBuffer( GL_ARRAY_BUFFER, cBuffer );
-    glVertexAttribPointer( GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0 );
+    glVertexAttribPointer( position, 3, GL_FLOAT, GL_FALSE, 12, 0 );
     
-    glEnableVertexAttribArray( GLKVertexAttribPosition );
-    glEnableVertexAttribArray( GLKVertexAttribColor );
-    glDrawArrays( GL_TRIANGLE_STRIP, 0, 28 );
-    glDisableVertexAttribArray( GLKVertexAttribPosition );
-    glDisableVertexAttribArray( GLKVertexAttribColor );
+    glBindBuffer( GL_ARRAY_BUFFER, cBuffer );
+    glVertexAttribPointer( normal, 3, GL_FLOAT, GL_FALSE, 12, 0 );
+    
+    glEnableVertexAttribArray( position );
+    glEnableVertexAttribArray( normal );
+    glDrawArrays( GL_TRIANGLES, 0, 36 );
+    glDisableVertexAttribArray( normal );
+    glDisableVertexAttribArray( position );
 }
 
 @end
